cb8123f732260db50c2bf9b382de865138d11773
vios@ylab <mori@.(none)>
2011-07-01 17:58:53 +0900

    perlを追加

---
+++ 20110701/perl/readme.txt
@@ -0,0 +1,13 @@
+﻿Perlによる文字列置換
+
+[l3-6]
+指定変数に標準入力から文字列代入するサブルーチン。
+chomp()で改行削除。
+
+[l22,33]
+open(IN, $inputFileName) || die "Error: $inputFileName\n";
+こう書くとファイルオープンに失敗した場合に
+エラーメッセージ出してくれる。
+if(!open(IN, $inputFileName)){...
+と同じカンジ。
+
---
+++ 20110701/perl/replace.pl
@@ -0,0 +1,41 @@
+﻿
+# 文字列入力サブルーチン
+sub inputString{
+	$_[0] = <STDIN>;
+	chomp( $_[0] );
+}
+
+# コマンド引数チェック
+if($ARGV[0] eq ""){
+	print STDERR "please input file name.\n";
+	print STDERR "usage : replace.pl <file name>";
+	exit(0);
+}
+
+# 変数設定
+my $inputFileName = $ARGV[0];
+my $outputFileName = "";
+my $targetString = "";
+my $newString = "";
+my $line = "";
+
+open(IN, $inputFileName) || die "Error: $inputFileName\n";
+
+print "please input target string : ";
+&inputString( $targetString );
+
+print "please input new string : ";
+&inputString( $newString );
+
+print "please input new file name : ";
+&inputString( $outputFileName );
+
+open(OUT, ">$outputFileName") || die "Error: $outputFileName\n"; #上書きモード
+
+while($line = <IN>){
+	$line =~ s/$targetString/$newString/g;
+	print OUT $line;
+}
+
+close(IN);
+close(OUT);
